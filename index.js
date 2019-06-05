29bf059b378ea39c0af0689d85bbafbdf4073944
Antonio Manuel Perez Lopez <antonio.perez@podgroup.com>
2019-06-05 20:20:09 +0200

    First version

--- index.js
+++ index.js
@@ -2,18 +2,31 @@ const tf = require('@tensorflow/tfjs-node-gpu')
 const data = require('./data')
 const model = require('./model')
 
-const run = async ({ epochs, batchSize, modelSavePath }) => {
-  // Load model.
-  // ex:
-  // await data.loadData()
-  console.log('------------------------')
-  console.log('Loading Data')
+const run = async ({ epochs, batchSize }) => {
   await data.loadData()
-  const {images: trainImages, labels: trainLabels} = data.getTrainData();
-  console.log('trainImages', trainImages)
-  console.log('trainLabels', trainLabels)
+  const {images: trainImages, labels: trainLabels} = data.getTrainData()
   model.summary()
+
+
+  const validationSplit = 0.15;
+  await model.fit(trainImages, trainLabels, {
+    epochs,
+    batchSize,
+    validationSplit
+  })
+
+  const {images: testImages, labels: testLabels} = data.getTestData()
+  const evalOutput = model.evaluate(testImages, testLabels)
+
+  let loss = evalOutput[0].dataSync()[0].toFixed(3)
+  let accuracy = evalOutput[1].dataSync()[0].toFixed(3)
+  console.log('----------------------------')
+  console.log(`    Lost: ${loss}`)
+  console.log(`Accuracy: ${accuracy}`)
+  console.log('----------------------------')
+  await model.save(`file://model.fit`);
+
 }
 
-run({epochs: 50, batchSize: 10, modelSavePath: './'})
+run({epochs: 1, batchSize: 128})
 
